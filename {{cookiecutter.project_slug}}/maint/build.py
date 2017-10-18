d91eecbca66f7e79a41005cc4e385ebd81b193e2
Michael V. DePalatis <mike@depalatis.net>
2017-10-18 13:38:25 -0400

    Update build script

--- {{cookiecutter.project_slug}}/maint/build.py
+++ {{cookiecutter.project_slug}}/maint/build.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+
 import os
-from subprocess import Popen
+import shutil
+from subprocess import check_call
 import shlex
 
-py_versions = ["2.7", "3.5", "3.6"]
-cmd = "conda build --python {version:s} conda.recipe"
+try:
+    shutil.rmtree('build')
+    os.mkdir('build')
+except OSError:
+    pass
 
-for version in py_versions:
-    cmd_str = cmd.format(version=version)
-
-    env = os.environ.copy()
-    env["PYTHON_BUILD_VERSION"] = version
-
-    print("Building for Python", version)
-    print(cmd_str)
-    Popen(shlex.split(cmd_str), env=env).wait()
+build_cmd = "conda build conda.recipe --output-folder build/"
+print(build_cmd)
+check_call(shlex.split(build_cmd))
